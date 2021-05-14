955f1d0cecdfe82841347a94df6c21d47c289c78
jzh04 <w2027@e-mirim.hs.kr>
2021-05-14 09:05:29 +0900

    문제 4번 스타일의 swap

---
+++ 문제04.py
@@ -0,0 +1,20 @@
+def solution(arr):
+    left = 0
+    right = len(arr)-1
+    #todo : @@@ 빈칸채우기
+    while @ @ @:
+        # arr[left]과 arr[right]의 값을 서로 바꿈
+        temp = arr[left]
+        arr[left] = arr[right]
+        arr[right] = temp
+        left += 1
+        right -= 1
+    return arr
+
+
+# The following is code to output testcase.
+arr = [1, 4, 2, 3]
+ret = solution(arr)
+
+# Press Run button to receive output.
+print("Solution: return value of the function is ", ret, " .")
