60209d8022de95c295736fbd78e7b418846be177
JiHamKim <w2027@e-mirim.com>
2021-05-21 09:03:32 +0900

    문제 05 보조문제

---
+++ 문제05.py
@@ -0,0 +1,11 @@
+def solution(number):
+   count = 0
+   for i in range(1, number + 1):  # for(int i=1; i<number+1)
+       current = i
+       temp = count
+       while current != 0:
+           if @@@:
+               count += 1
+               print("pair", end = '') # 디버깅을 위한 출력(없어도 무관)
+           current = current // 10
+   return count
