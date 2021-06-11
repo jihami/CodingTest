88f72cc6a4b44e618f2fe8c60365e01142ef56a5
jzh04 <w2027@e-mirim.hs.kr>
2021-06-11 10:20:51 +0900

    lv1-04 가운데 글자 가져오기 완성

---
+++ 프로그래머스/04 가운데 글자 가져오기.py
@@ -0,0 +1,16 @@
+# 가운데 글자 가져오기
+def solution(s):
+    # 문자열의 갯수가 홀수일 때는(한개만 반환)
+    # 5개일때 -> idx:2 / 7개일때 -> idx:3 / 9개일때 -> idx:4
+    if len(s) % 2 == 1:
+        return s[len(s) // 2]
+
+    # 문자열의 갯수가 짝수일 때는(두개를 반환)
+    # 4개일때 idx:1,2 / 6개일때 idx:2,3 / 8개일때 idx:3,4
+    else:
+        return s[len(s)//2-1] + s[len(s)//2]
+
+s1 = "abcde"
+print(solution(s1))
+s2 = "qwer"
+print(solution(s2))
