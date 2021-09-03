85420d257e9e406e84da686c2c2af4cc846b4d47
jzh04 <w2027@e-mirim.hs.kr>
2021-09-03 10:02:41 +0900

    ~13

--- 간단한문제.py
+++ 간단한문제.py
@@ -51,4 +51,35 @@ print(movie_rank)
 
 movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
 del movie_rank[-2]
+# for x in movie_rank:
+#     if x == "럭키":
+#         movie_rank.remove("럭키")
 print(movie_rank)
+
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨', '럭키']
+result = list()
+for x in movie_rank: #중복 허용 x
+    if x != "럭키":
+        result.append(x)
+    else:
+        continue
+print(result)
+
+nums = [1, 2, 3, 4, 5]
+print(sum(nums))
+
+cook = ["피자", "김밥", "만두", "양념치킨", "족발", "피자", "김치만두", "쫄면", "쏘세지", "라면", "팥빙수", "김치전"]
+# print(len(cook)) #중복 허용 ver
+a=set(cook) #set이 중복 제거해줌
+print(len(a))
+resultc = list() #반복분 코드
+for x in cook:
+    if x not in resultc:
+        resultc.append(x)
+    else:
+        continue
+ac=len(resultc)
+print(ac)
+
+nums = [1, 2, 3, 4, 5]
+print(sum(nums)/len(nums))
