04b616d1b402a5edb020ce5281944a4906bb63f9
jzh04 <w2027@e-mirim.hs.kr>
2021-09-03 09:06:30 +0900

    8,9,10 이지롱~!~!~~

--- 간단한문제.py
+++ 간단한문제.py
@@ -40,3 +40,15 @@ print(int(sam))
 ticker = "btc_krw"
 result = ticker.split("_")
 print(result)
+
+movie_rank = ["닥터 스트레인지", "스플릿", "럭키"]
+movie_rank.append("배트맨")
+print(movie_rank)
+
+movie_rank = ['닥터 스트레인지', '스플릿', '럭키', '배트맨']
+movie_rank.insert(1,"슈퍼맨")
+print(movie_rank)
+
+movie_rank = ['닥터 스트레인지', '슈퍼맨', '스플릿', '럭키', '배트맨']
+del movie_rank[-2]
+print(movie_rank)
