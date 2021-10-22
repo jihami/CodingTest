fd52255677b1e309c3614f14b75ab1056f9b2a47
jihami <w2027@e-mirim.hs.kr>
2021-10-22 10:04:35 +0900

    문제5.py - 방법 2

--- algorithm/문제5.py
+++ algorithm/문제5.py
@@ -13,13 +13,29 @@ ex) 빈 리스트에서 [1, 3, 2, 6, 4, 9, 5]가 추가되는 상황
 # print(count)
 
 '''지우쌤'''
-#방법1
+''''#방법1
 arr = [1, 3, 2, 6, 4, 9, 5]
 result = list()
 count = 0
 max = -1
 
 #arr에 있는 모든 요소 x룰 순서대로 반복
+for x in arr:
+    # x값이 max값보다 크다면
+    if x > max:
+        #max값 x로 갱신
+        max = x
+        #count 1증가
+        count += 1
+print(count)'''
+
+#방법2
+arr = [1, 3, 2, 6, 4, 9, 5]
+result = list()
+count = 1
+max = arr[0]
+
+#arr에 있는 일 [1]부터의 요소x를 순서대로 반복
 for x in arr:
     # x값이 max값보다 크다면
     if x > max:
