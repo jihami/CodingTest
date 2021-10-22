834e4160f729b28aa0d9eaba43283051df3e2d93
jihami <w2027@e-mirim.hs.kr>
2021-10-22 10:01:13 +0900

    문제5.py - 방법 1

--- algorithm/문제5.py
+++ algorithm/문제5.py
@@ -2,12 +2,29 @@
 (값의 범위는 0~100이다.)
 ex) 빈 리스트에서 [1, 3, 2, 6, 4, 9, 5]가 추가되는 상황
 '''
-list_1 = []
-list_2 = [1, 3, 2, 6, 4, 9, 5]
-count = 1
-for i in range(len(list_2)):
-    list_1.append(list_2[i])
-    if(list_1[i] > list_1[i-1]):
+# list_1 = []
+# list_2 = [1, 3, 2, 6, 4, 9, 5]
+# count = 1
+# for i in range(len(list_2)):
+#     list_1.append(list_2[i])
+#     if(list_1[i] > list_1[i-1]):
+#         count += 1
+# print(list_1)
+# print(count)
+
+'''지우쌤'''
+#방법1
+arr = [1, 3, 2, 6, 4, 9, 5]
+result = list()
+count = 0
+max = -1
+
+#arr에 있는 모든 요소 x룰 순서대로 반복
+for x in arr:
+    # x값이 max값보다 크다면
+    if x > max:
+        #max값 x로 갱신
+        max = x
+        #count 1증가
         count += 1
-print(list_1)
 print(count)
