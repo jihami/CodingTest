b4a304cdda064b1783d5d4da8d5c5509a420b181
jihami <w2027@e-mirim.hs.kr>
2021-10-29 10:20:12 +0900

    문제6.py - 방법 1

--- algorithm/문제6.py
+++ algorithm/문제6.py
@@ -3,9 +3,15 @@ ex) ILOVKEFLOVEE에서 LOV가 몇개 있는지 카운팅'''
 string = "ILOVKEFLOVEE"
 find = "LOV"
 count = 0
+
+#Q1. 일반화된 코드가 아닌이유 : find 문자열의 길이에 따라 코드가 달라짐
+
+#Q2. 결함 : i+1 ... 떄문에 인덱스 길이를 넘어갈 수 있음
+
 #i는 0부터 len(string)-1 만큼 반복
 for i in range(len(string)):
-    if string[i] == "L":
+    # if string[i] == "L": Q2 결함
+    if string[i] == "L" and i+len(find) <= len(string):
         if string[i+1] == "O":
             if string[i+2] == "V":
                 count += 1
